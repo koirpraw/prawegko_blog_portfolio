4df3310df2c154225c3023fa8db24e7f1caa46ae
Praweg <koirpraw@gmail.com>
2023-03-01 00:20:24 -0700

    blog notion-cms configured, blog page updated

---
+++ src/components/blog.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Link from 'next/link';
+import { getAllPublished } from '@/lib/notion';
+import styles from '../styles/Home.module.css'
+
+export default function Blog({posts}) {
+  if(!posts) return <h1>No posts</h1>
+    return (
+        <div className={styles.container}>
+           <h1 className={styles.pageTitle}> 🌐 Blog</h1>
+           {posts.map((post, index) => (
+          <section key={index} className={styles.card}>
+            <div>
+                <Link href={`/posts/${post.slug}`}>
+                <a className={styles.action}>{post.title}</a>
+                </Link>
+                <div>
+                    {post.date}
+                </div>
+            </div>
+            <p>
+              {post.description}
+            </p>
+          </section>
+        ))}
+        </div>
+    )
+    }
+
+    export const getStaticProps = async()=>{
+        const data = await getAllPublished()
+            return{
+                props:{
+                    posts: data
+                },
+                revalidate: 60
+            };
+        
+    }
