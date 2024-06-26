5b10e6702a3a9081fdb1e441a91a8a5ea8b3f2e3
Praweg <koirpraw@gmail.com>
2024-06-25 22:37:42 -0600

    Blog styling and layout fixes responsive design

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -2,16 +2,17 @@
 import Head from "next/head";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Container } from "@/components/container"
 
 import { vscDarkPlus, dracula, } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import { Button } from "@/components/ui/button";
 import { getAllPublished, getSingleBlogPostBySlug } from "@/lib/notion.js";
 import Link from 'next/link';
-
+import Text from "@/components/notion_blocks/text";
+// import hljs from 'highlight.js/lib/core'
 
 
 const CodeBlock = ({ language, codestring }) => {
-
 
   return (
     <SyntaxHighlighter language={language} style={dracula} PreTag="div">
@@ -21,9 +22,6 @@ const CodeBlock = ({ language, codestring }) => {
 }
 
 const Post = ({ post }) => {
-
-
-
   const colorClasses = {
     red: 'bg-red-200',
     yellow: 'bg-yellow-200',
@@ -34,8 +32,8 @@ const Post = ({ post }) => {
 
 
   return (
-    <>
 
+    <div className="w-full lg:px-48">
       <Head>
         <title>{post.metadata.title}</title>
         <meta name="description" content={post.metadata.description} />
@@ -48,67 +46,69 @@ const Post = ({ post }) => {
 
       </Head>
 
+      <div className="max-w-2xl min-w-full flex flex-col justify-evenly ">
+        <article>
 
 
-      <div className="flex flex-col justify-center items-center space-y-6 min-h-screen lg:mx-48 mx-6 ">
-
-        <h2 className="text-2xl font-bold my-12">{post.metadata.title}</h2>
-
-        <div className="flex flex-col justify-evenly space-y-6 shadow-lg rounded-lg p-12 dark:shadow-lime-100">
 
 
-          <div className="flex flex-row justify-between items-center py-4 lg:px-12 px-4 border-2 rounded-md">
-            {/* <p >Author: Praweg Koirala</p> */}
-            <a href="/" target="_blank"><p>Author: Praweg Koirala</p></a>
-            <p >🗓️  {post.metadata.date}</p>
-            <p>⌛️  {post.metadata.readTime} mins</p>
-            <div className={` px-2  rounded-md text-sm font-medium ${colorClasses[post.metadata.phaseColor]}`} > {post.metadata.phase} </div>
+          <div className="flex flex-col justify-evenly space-y-6 shadow-xl rounded-xl lg:p-12 p-4 dark:shadow-lime-100">
+
+            <div className="flex justify-center items-center">
+              <h2 className="mb-4 font-bold text-3xl text-zinc-800 dark:text-zinc-100 ">
+                {post.metadata.title}
+              </h2>
+            </div>
+
+            <div className="flex lg:flex-row flex-col lg:justify-between  items-start lg:items-center py-4 px-2 lg:px-12  border-2 rounded-md">
+
+              <a href="/" target="_blank"><p>Author: Praweg Koirala</p></a>
+              <p >🗓️  {post.metadata.date}</p>
+              <p>⌛️  {post.metadata.readTime} mins</p>
+              <div className={` px-2  rounded-md text-sm font-medium ${colorClasses[post.metadata.phaseColor]}`} > {post.metadata.phase} </div>
+              <p className="font-small"> 🏷️ &nbsp; &nbsp;{post.metadata.tags.join(' | ')}</p>
+            </div>
 
 
-            <p className="font-medium"> 🏷️ &nbsp; &nbsp;{post.metadata.tags.join(' | ')}</p>
-          </div>
-
-          <div className="flex flex-col justify-center items-center mx-6  space-y-4" >
             <ReactMarkdown
-
               components={{
+                // code({ node, children, ...props }) {
+                //   return <p className=" font-mono bg-red-200" {...props}>{children}</p>
+                // },
+                CodeBlock({ node, children, ...props }) {
+                  return <div className="whitespace-pre max-w-full overflow-x-auto bg-slate-500 p-4 rounded-xl" {...props}>{children}</div>
+                },
                 code({ node, inline, className, children, ...props }) {
+
+
+
                   const match = /language-(\w+)/.exec(className || '')
                   return !inline && match ? (
                     <CodeBlock
                       codestring={String(children).replace(/\n$/, '')}
                       language={match[1]}
 
+
+
+
                     />
                   ) : (
-                    <code className={className} {...props}>
+                    <code
+                      // className="text-green"
+                      // dangerouslySetInnerHTML={{ __html: post.markdown }}
+                      {...props}>
+
                       {children}
                     </code>
                   )
                 },
-                h1({ node, children, ...props }) {
-                  return <h1 className="text-bold text-3xl" {...props}>{children}</h1>
-                },
-                h2({ node, children, ...props }) {
-                  return <h2 className="text-medium text-2xl" {...props}>{children}</h2>
-                },
-                h3({ node, children, ...props }) {
-                  return <h2 className="font-medium text-xl" {...props}>{children}</h2>
-                },
-                p({ node, children, ...props }) {
-                  return <p className="font-light text-lg"{...props}>{children}</p>
-                },
-                ol({ node, children, ...props }) {
-                  return <div className="text-lg" {...props}>{children}</div>
-                },
-                script({ node, children, ...props }) {
-                  return <p className="text-red-400 font-mono" {...props}>{children}</p>
-                },
-                col({ node, children, ...props }) {
-                  return
-                }
               }}
-            >{post.markdown}</ReactMarkdown>
+
+            // dangerouslySetInnerHTML={{ __html: post.markdown }}
+            // children={post.markdown}
+            >
+              {post.markdown}
+            </ReactMarkdown>
 
             <div className="flex justify-center text-gray-400 space-x-2">
               <p>This post was last updated on:  {post.metadata.updatedDate === "" ? post.metadata.date : post.metadata.updatedDate}</p>
@@ -122,14 +122,28 @@ const Post = ({ post }) => {
 
           </div>
 
+        </article>
+        <div className="flex justify-center items-center pt-16">
+          <Button asChild>
+            <Link href="/blogPage">Back</Link>
+          </Button>
 
         </div>
-        <Button asChild>
-          <Link href="/blogPage">Back</Link>
-        </Button>
+
+
+
 
       </div>
-    </>
+
+
+
+      {/* 
+      </div> */}
+      {/* </Container> */}
+
+    </div>
+
+
 
   );
 };
--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -3,52 +3,11 @@
 import Head from "next/head";
 import Project from "@/components/project";
 import TransitionHeader from "@/components/transitionHeader";
+import { projects } from "@/data/projects_data";
 
 
 
 function projectsPage() {
-
-
-  const projects = [
-    {
-      title: 'AI Chatbot App, Flutter',
-      description:
-        'A Clone of Bing Co-Pilot app built with Flutter using Open AI Api. ',
-      techStack: ['Flutter', 'OpenAI'],
-      image: "https://pk-web-assets.s3.amazonaws.com/gitub-feature-template2.png",
-      link: 'https://prawegko.dev',
-      github: 'https://github.com/koirpraw/bing_copilot_ai_clone_flutter',
-    },
-    {
-      title: 'GoatReport- A Fitness Tracking app, Flutter',
-      description:
-        'UI clone of Fitness Tracking app called Metriport. Uses SynFusion Library for Data Visulaization. A great example to implement UX for data heavy applications. ',
-      techStack: ['Flutter', 'GetX', 'Sync Fusion'],
-      image: "https://images.unsplash.com/photo-1710959781827-5fe4b8f058b2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: '',
-      github: 'https://github.com/koirpraw/goatreport',
-    },
-    {
-      title: 'MERN Stack, Job Finder/Job Listing App, Web Application',
-      description:
-        'A full Stack App with MongoDB, Express JS, React & Node JS. Job Listing App that allows to create new Job listing,and edit/delete posting',
-      techStack: ['MongoDB', 'Express JS', 'React', 'Node JS'],
-      image: "https://images.unsplash.com/photo-1710959781827-5fe4b8f058b2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: '',
-      github: 'https://github.com/koirpraw/bing_copilot_ai_clone_flutter',
-    },
-    {
-      title: 'Task Management App ',
-      description:
-        'Task Management app that uses SQLite for persistent local storage. GetX State management.Lottie Animations',
-      techStack: ['Flutter', 'GetX', 'SQFLite', "Lottie"],
-      image: "https://images.unsplash.com/photo-1710959781827-5fe4b8f058b2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: '',
-      github: 'https://github.com/koirpraw/bing_copilot_ai_clone_flutter',
-    },
-  ]
-
-
 
 
   return (
@@ -68,7 +27,7 @@ function projectsPage() {
 
 
 
-          {projects.map((project, index) => (
+          {projects && projects.map((project, index) => (
             <Project key={index}
               title={project.title}
               description={project.description}
